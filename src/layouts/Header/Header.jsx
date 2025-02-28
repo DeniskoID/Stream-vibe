6f34e0ea43866c208024011e850f6fb574d1f15d
Denis <denis-pog.78@yandex.com>
2025-02-28 17:58:24 +0300

    feat: add initial assets and components for the project. Create a Header with components and js script for a burger menu

--- src/layouts/Header/Header.jsx
+++ src/layouts/Header/Header.jsx
@@ -1,5 +1,80 @@
-const Header = () => {
-  return <header>HEADER</header>
+import "./Header.scss"
+import classNames from "classnames"
+import Logo from "@/components/Logo"
+import Button from "@/components/Button"
+import BurgerButton from "@/components/BurgerButton"
+
+const Header = (props) => {
+  const { url } = props
+
+  const menuItems = [
+    {
+      label: "Home",
+      href: "/",
+    },
+    {
+      label: "Movies & Shows",
+      href: "/movies",
+    },
+    {
+      label: "Support",
+      href: "/support",
+    },
+    {
+      label: "Subscriptions",
+      href: "/subscribtions",
+    },
+  ]
+
+  return (
+    <header className="header" data-js-ovarlay-menu="">
+      <div className="header__inner container">
+        <Logo className="header__logo" loading="eager" />
+        <dialog
+          className="header__overlay-menu-dialog"
+          data-js-ovarlay-menu-dialog=""
+        >
+          <nav className="header__menu">
+            <ul className="header__menu-list">
+              {menuItems.map(({ label, href }, index) => (
+                <li className="header__menu-item" key={index}>
+                  <a
+                    className={classNames("header__menu-link", {
+                      "is-active": url === href,
+                    })}
+                    href={href}
+                  >
+                    {label}
+                  </a>
+                </li>
+              ))}
+            </ul>
+          </nav>
+          <div className="header__actions">
+            <Button
+              className="header__button"
+              label="Search"
+              isLabelHidden
+              mode="transparent"
+              iconName="search"
+            />
+
+            <Button
+              className="header__button"
+              label="Notification"
+              isLabelHidden
+              mode="transparent"
+              iconName="notification"
+            />
+          </div>
+        </dialog>
+        <BurgerButton
+          className="header__burger-button visible-tablet"
+          extraAttrs={{ "data-js-ovarlay-menu-burger-button": "" }}
+        />
+      </div>
+    </header>
+  )
 }
 
 export default Header
