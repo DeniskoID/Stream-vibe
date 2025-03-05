9bb4229c7824da4818cbe2441cffa1edd36b8f54
Denis <denis-pog.78@yandex.com>
2025-03-05 17:14:42 +0300

    feat(components): implement Accordion system with FAQ section

---
+++ src/sections/Questions/Questions.jsx
@@ -0,0 +1,32 @@
+import Section from "@/layouts/Section"
+import Button from "@/components/Button"
+import AccordionGroup from "@/components/AccordionGroup"
+import Accordion from "@/components/Accordion"
+import FAQData from "./FAQData"
+
+const Questions = () => {
+  return (
+    <Section
+      title="Frequently Asked Questions"
+      titleId="questions-title"
+      description="Got questions? We've got answers! Check out our FAQ section to find answers to the most common questions about StreamVibe."
+      actions={<Button label="Ask a Question" href="/support" />}
+    >
+      <AccordionGroup columns={2}>
+        {FAQData.map((item, index) => (
+          <Accordion
+            title={item.question}
+            id={`question-${index}`}
+            name="questions"
+            isOpen={index === 0}
+            key={index}
+          >
+            <p>{item.answer}</p>
+          </Accordion>
+        ))}
+      </AccordionGroup>
+    </Section>
+  )
+}
+
+export default Questions
